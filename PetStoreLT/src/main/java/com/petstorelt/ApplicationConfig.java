bf35c93b0a9228770313f89d66db6ba3e0a2cf29
lauthou <lauthou@free.fr>
2016-03-25 14:48:54 +0100

    Change-Id: I3d3563a5c6ac9d89cdb978ceb859b41d85f2179d

--- PetStoreLT/src/main/java/com/petstorelt/ApplicationConfig.java
+++ PetStoreLT/src/main/java/com/petstorelt/ApplicationConfig.java
@@ -10,19 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan
 @ImportResource("classpath:context.xml")
 public class ApplicationConfig  extends WebMvcConfigurerAdapter {
-
-//	@Override
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//	    registry.addViewController("/").setViewName("redirect:/templates/index.html");
-//	    registry.addViewController("/PetStoreLT").setViewName("redirect:/index.html");
-//	}
 	
 	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
-////        registry.addViewController("/home").setViewName("home");
-////        registry.addViewController("/").setViewName("home");
-////        registry.addViewController("/index").setViewName("index");
-//		 registry.addViewController("/").setViewName("redirect:/templates/index.html");
         registry.addViewController("/login").setViewName("forward:/templates/login.html");
         registry.addViewController("/admin").setViewName("forward:/templates/index.html");
     }
